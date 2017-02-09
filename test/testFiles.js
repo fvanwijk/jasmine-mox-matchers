ca41d6328039784017daa595897c1095edba7b2a
Frank van Wijk <f.e.vanwijk@gmail.com>
2017-02-09 14:37:49 +0100

    Bump all dependencies

--- test/testFiles.js
+++ test/testFiles.js
@@ -5,7 +5,7 @@ module.exports = [
       'node_modules/lodash/lodash.js',
       'node_modules/angular/angular.js',
       'node_modules/angular-mocks/angular-mocks.js',
-      'node_modules/testception/src/testception.js'
+      'node_modules/testception/dist/testception.min.js'
     ]
   },
   {
