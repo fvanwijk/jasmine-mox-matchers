89e17f40e6c160b89306ca134b35d4490b89bc44
Frank van Wijk <f.e.vanwijk@gmail.com>
2017-02-09 14:38:27 +0100

    Clean up Wallaby config
    There is no XHR involved

--- wallaby.conf.js
+++ wallaby.conf.js
@@ -4,12 +4,6 @@ module.exports = function () {
   return {
     //testFramework: 'jasmine@1.3.1',
     files: wallabyFiles.files,
-    tests: wallabyFiles.tests,
-    env: {
-      runner: require('phantomjs2-ext').path,
-      params: {
-        runner: '--web-security=false'
-      }
-    }
+    tests: wallabyFiles.tests
   };
 };
