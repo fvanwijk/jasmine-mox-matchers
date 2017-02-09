278639c9a273a117c6349e715ffd5307a89eeb50
Frank van Wijk <f.e.vanwijk@gmail.com>
2017-02-09 16:53:08 +0100

    Further clean up karma config

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,6 @@ var karmaFiles = require('test-runner-config').getKarmaFiles(require('./testFile
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
     frameworks: ['jasmine'],
     preprocessors: {
       'src/jasmine-mox-matchers.js': ['coverage']
@@ -10,13 +9,8 @@ module.exports = function (config) {
     files: karmaFiles.files,
     exclude: karmaFiles.exclude,
     reporters: ['progress', 'coverage'],
-    port: 8080,
-    runnerPort: 9100,
-    colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: false,
     browsers: ['PhantomJS'],
-    captureTimeout: 5000,
     singleRun: true,
     coverageReporter: {
       dir: 'coverage',
